6a66a1f1a7258cb33bdcc2c09f7b0018fbb1089d
Shipon Roy <shiponworks@gmail.com>
2025-12-12 10:59:41 +0600

    Export and impourt complets

---
+++ Chapter 12 -Destructuring-Problem-solving/Export-import/import.js
@@ -0,0 +1,4 @@
+import student, { trepul } from './srcipt.js';
+
+console.log(student);
+console.log(trepul(5));
---
+++ Chapter 12 -Destructuring-Problem-solving/Export-import/srcipt.js
@@ -0,0 +1,6 @@
+const student = { name: 'Shipon Roy', age: 18, Roll: 9, class: 12 };
+export const trepul = num => {
+  return num * 3;
+};
+
+export default student;
